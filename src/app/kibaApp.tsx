b9e381957078973fef8920f3d342c22b0a9c39eb
Krishan Patel <krishan711@gmail.com>
2021-01-27 11:11:12 +0000

    .

--- src/app/kibaApp.tsx
+++ src/app/kibaApp.tsx
@@ -18,7 +18,7 @@ interface IKibaAppProps extends IMultiAnyChildProps {
 
 const StyledMainView = styled.div`
   min-height: 100vh;
-  /* NOTE(krishan711): the min-height doesn't propagate to children that have height:100% unless this is here */
+  /* NOTE(krishan711): the min-height doesn't propagate to children that have height:100% unless this is here (https://stackoverflow.com/questions/8468066) */
   height: 1px;
 `;
 
