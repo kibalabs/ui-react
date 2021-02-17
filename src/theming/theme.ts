9e293aa5566145ec561244f4f9bd6499a7d00277
MoiyadKayda <42097538+MoiyadKayda@users.noreply.github.com>
2021-02-18 04:20:09 +0530

    Feature - Created Dialog atom (#55)
    * .
    
    * .
    
    * example added for dialog
    
    * .
    
    * .
    
    * .
    
    * .
    
    * dialog moved to atoms
    
    * .
    
    * .
    
    * .
    
    * .
    
    Co-authored-by: Krishan Patel <krishan711@gmail.com>

--- src/theming/theme.ts
+++ src/theming/theme.ts
@@ -2,6 +2,7 @@ import { IBulletListTheme } from '../atoms/bulletList';
 import { IBulletTextTheme } from '../atoms/bulletText';
 import { IButtonTheme } from '../atoms/button';
 import { ICheckboxTheme } from '../atoms/checkbox';
+import { IDialogTheme } from '../atoms/dialog';
 import { IIconButtonTheme } from '../atoms/iconButton';
 import { IInputWrapperTheme } from '../atoms/inputWrapper';
 import { ILinePagerTheme } from '../atoms/linePager';
@@ -46,6 +47,7 @@ export interface ITheme extends ThemeType {
   bulletLists: ThemeMap<IBulletListTheme>,
   bulletTexts: ThemeMap<IBulletTextTheme>,
   buttons: ThemeMap<IButtonTheme>,
+  dialogs: ThemeMap<IDialogTheme>,
   checkboxes: ThemeMap<ICheckboxTheme>,
   linkBases: ThemeMap<ILinkBaseTheme>,
   iconButtons: ThemeMap<IIconButtonTheme>,
