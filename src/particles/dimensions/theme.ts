cc058a967142b4b3f9ce381207411ee30dcf083d
Moiyad <moiyadkayda@gmail.com>
2021-04-23 12:03:05 +0000

    .

--- src/particles/dimensions/theme.ts
+++ src/particles/dimensions/theme.ts
@@ -1,8 +1,3 @@
-import React from 'react';
-
-import { useWindowSize } from '@kibalabs/core-react';
-
-import { useDimensions } from '../../theming';
 import { ThemeType } from '../../util';
 
 export interface IDimensionGuide extends ThemeType {
@@ -95,31 +90,3 @@ export const getPaddingSize = (size: PaddingSizeProp, theme: IDimensionGuide): s
   console.error(`Failed to find padding size: ${size} (${fieldName})`);
   return '0px';
 };
-
-export const ResponsiveScreenSizeContext = React.createContext<ScreenSize>(ScreenSize.Base);
-
-const getScreenSizeValue = (size: ScreenSize, theme: IDimensionGuide): number => {
-  if (size === ScreenSize.Base) {
-    return 0;
-  }
-  return Number(getScreenSize(size as string as ScreenSize, theme).replace('px', ''));
-};
-
-export function useResponsiveScreenSize(): ScreenSize {
-  const dimensions = useDimensions();
-  const windowSize = useWindowSize();
-  let screenSize = ScreenSize.Base;
-  if (windowSize.width > getScreenSizeValue(ScreenSize.Small, dimensions)) {
-    screenSize = ScreenSize.Small;
-  }
-  if (windowSize.width > getScreenSizeValue(ScreenSize.Medium, dimensions)) {
-    screenSize = ScreenSize.Medium;
-  }
-  if (windowSize.width > getScreenSizeValue(ScreenSize.Large, dimensions)) {
-    screenSize = ScreenSize.Large;
-  }
-  if (windowSize.width > getScreenSizeValue(ScreenSize.ExtraLarge, dimensions)) {
-    screenSize = ScreenSize.ExtraLarge;
-  }
-  return screenSize;
-}
---
+++ src/util/useResponsiveScreenSize.ts
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { useWindowSize } from '@kibalabs/core-react';
+
+import { useDimensions } from '../theming';
+import { ScreenSize, IDimensionGuide, getScreenSize} from '../particles'
+
+export const ResponsiveScreenSizeContext = React.createContext<ScreenSize>(ScreenSize.Base);
+
+const getScreenSizeValue = (size: ScreenSize, theme: IDimensionGuide): number => {
+  if (size === ScreenSize.Base) {
+    return 0;
+  }
+  return Number(getScreenSize(size as string as ScreenSize, theme).replace('px', ''));
+};
+
+export function useResponsiveScreenSize(): ScreenSize {
+  const dimensions = useDimensions();
+  const windowSize = useWindowSize();
+  let screenSize = ScreenSize.Base;
+  if (windowSize.width > getScreenSizeValue(ScreenSize.Small, dimensions)) {
+    screenSize = ScreenSize.Small;
+  }
+  if (windowSize.width > getScreenSizeValue(ScreenSize.Medium, dimensions)) {
+    screenSize = ScreenSize.Medium;
+  }
+  if (windowSize.width > getScreenSizeValue(ScreenSize.Large, dimensions)) {
+    screenSize = ScreenSize.Large;
+  }
+  if (windowSize.width > getScreenSizeValue(ScreenSize.ExtraLarge, dimensions)) {
+    screenSize = ScreenSize.ExtraLarge;
+  }
+  return screenSize;
+}
