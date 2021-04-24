6eb6b53d7017d7e0766617df9e9d4b66ee3fb2c4
Moiyad <moiyadkayda@gmail.com>
2021-04-24 10:38:33 +0000

    .

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,5 @@
 export * from './componentUtil';
 export * from './responsiveUtil';
-export * from './useResponsiveScreenSize';
+// export * from './useResponsiveScreenSize';
 export * from './stringUtil';
 export * from './theme';
