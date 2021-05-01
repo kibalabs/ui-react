f90d34dd8e3d66f49478e16a1dcb0b64e6d27e17
Moiyad <moiyadkayda@gmail.com>
2021-05-01 11:06:02 +0000

    .

--- src/particles/text/component.tsx
+++ src/particles/text/component.tsx
@@ -103,7 +103,7 @@ export const Text = (props: ITextProps): React.ReactElement => {
   return (
     <StyledText
       id={props.id}
-      className={getClassName(Text.displayName, props.className, clipToLines == 1 && 'singleLine' , clipToLines >= 2 && 'fixedLines')}
+      className={getClassName(Text.displayName, props.className, clipToLines === 1 && 'singleLine', clipToLines >= 2 && 'fixedLines')}
       theme={theme}
       alignment={props.alignment}
       clipToLines={clipToLines}
