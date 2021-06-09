f4e24e6139d11eb89e8488c5d471c843a67f6be4
Krishan Patel <krishan711@gmail.com>
2021-06-09 15:17:49 +0100

    .

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,11 +8,11 @@ module.exports = {
   ],
   typescript: {
     check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
+    // checkOptions: {},
+    // reactDocgen: 'react-docgen-typescript',
+    // reactDocgenTypescriptOptions: {
+    //   shouldExtractLiteralValuesFromEnum: true,
+    //   propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    // },
   },
 };
