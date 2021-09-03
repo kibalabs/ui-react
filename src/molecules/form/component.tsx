93dec7165fdca7d8ce5a438c95d70b6e52df038e
Moiyad <moiyadkayda@gmail.com>
2021-09-03 18:03:03 +0530

    .

--- src/molecules/form/component.tsx
+++ src/molecules/form/component.tsx
@@ -25,7 +25,7 @@ const LoadingOverlay = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  background-color: rgba(0, 0, 0, 0.7);
+  background-color: rgba(0, 0, 0, 0.05);
   z-index: 5;
   display: flex;
   flex-direction: column;
