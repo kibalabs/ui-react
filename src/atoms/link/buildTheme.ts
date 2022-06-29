6225786d67095975b9ba9b38c188bbe278a4e8f7
Krishan Patel <krishan711@gmail.com>
2022-06-29 23:16:49 +0100

    Feature: Make links inherit style by default (#154)
    * .
    
    * .
    
    * .

--- src/atoms/link/buildTheme.ts
+++ src/atoms/link/buildTheme.ts
@@ -1,14 +1,14 @@
 import { RecursivePartial } from '@kibalabs/core';
 
 import { IBoxTheme, IColorGuide, IDimensionGuide, ITextTheme } from '../../particles';
-import { mergeTheme, mergeThemePartial, ThemeMap } from '../../util';
+import { mergeTheme, ThemeMap } from '../../util';
 import { ILinkTheme } from './theme';
 
 export const buildLinkThemes = (colors: IColorGuide, dimensions: IDimensionGuide, textThemes: ThemeMap<ITextTheme>, boxThemes: ThemeMap<IBoxTheme>, base?: RecursivePartial<Record<string, ILinkTheme>>): ThemeMap<ILinkTheme> => {
   const defaultLinkTheme = mergeTheme<ILinkTheme>({
     normal: {
       default: {
-        text: mergeTheme(textThemes.default, {
+        text: mergeTheme(textThemes.inherit as ITextTheme, {
           color: '$colors.brandPrimary',
           'text-decoration': 'underline',
         }),
@@ -37,20 +37,20 @@ export const buildLinkThemes = (colors: IColorGuide, dimensions: IDimensionGuide
     },
   }, base?.default);
 
-  const inheritLinkTheme = mergeThemePartial<ILinkTheme>({
-    normal: {
-      default: {
-        text: mergeThemePartial(textThemes.inherit, {
-          color: '$colors.brandPrimary',
-          'text-decoration': 'underline',
-        }),
-      },
-    },
-  }, base?.default);
+  // const inheritLinkTheme = mergeThemePartial<ILinkTheme>({
+  //   normal: {
+  //     default: {
+  //       text: mergeThemePartial(textThemes.inherit, {
+  //         color: '$colors.brandPrimary',
+  //         'text-decoration': 'underline',
+  //       }),
+  //     },
+  //   },
+  // }, base?.default);
 
   return {
     ...(base || {}),
     default: defaultLinkTheme,
-    inherit: inheritLinkTheme,
+    inherit: defaultLinkTheme,
   };
 };
