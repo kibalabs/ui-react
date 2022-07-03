37d66f072a5948e8d124de739c335615ad4d0530
Krishan Patel <krishan711@gmail.com>
2022-07-03 15:18:41 +0100

    Fix: correct alternateColors calculation + add placeholderText to OptionSelect (#155)
    * .
    
    * .
    
    * .

--- src/particles/colors/buildTheme.ts
+++ src/particles/colors/buildTheme.ts
@@ -39,41 +39,38 @@ export const buildColors = (base?: Partial<IColorGuide>): IColorGuide => {
 };
 
 export const buildAlternateColors = (colors: IColorGuide, base?: RecursivePartial<Record<string, Partial<IColorGuide>>>): Record<string, IColorGuide> => {
-  const inverseBase = base?.inverse;
+  const output: Record<string, IColorGuide> = {};
+
+  const inverseBase = base?.inverse || {} as Partial<IColorGuide>;
   const brandPrimary = inverseBase?.brandPrimary || colors.brandPrimary;
-  const brandSecondary = inverseBase?.brandSecondary || colors.brandSecondary;
   const background = inverseBase?.background || invert(colors.background);
   const text = inverseBase?.text || getLuminance(background) > 0.5 ? '#222222' : '#eeeeee';
   const textOnBrand = inverseBase?.textOnBrand || getLuminance(brandPrimary) > 0.5 ? '#222222' : '#eeeeee';
-  const disabled = inverseBase?.disabled || colors.disabled;
-  const disabledText = inverseBase?.disabledText || colors.disabledText;
-  const error = inverseBase?.error || colors.error;
-  const success = inverseBase?.success || colors.success;
-
-  const inverseColors: IColorGuide = {
+  output.inverse = expandColors({
     ...colors,
     ...base?.inverse,
     brandPrimary,
-    brandSecondary,
     background,
     text,
     textOnBrand,
-    disabled,
-    disabledText,
-    error,
-    success,
-  };
+  });
 
-  return {
-    ...(base || {}),
-    inverse: expandColors(inverseColors),
-  };
+
+  Object.keys(base || {}).forEach((alternateColorName: string): void => {
+    if (alternateColorName === 'inverse' || !base || !base[alternateColorName]) {
+      return;
+    }
+    output[alternateColorName] = expandColors(base[alternateColorName] as Partial<IColorGuide>);
+  });
+
+  return output;
 };
 
-const expandColors = (colors: IColorGuide, base?: IColorGuide): IColorGuide => {
+const expandColors = (colors: Partial<IColorGuide>): IColorGuide => {
   const values = [0.01, 0.05, 0.10, 0.20, 0.25, 0.50, 0.75, 0.80, 0.90, 0.95];
   const expandedColors = Object.keys(colors).reduce((extendedColors: Partial<IColorGuide>, colorKey: string): Partial<IColorGuide> => {
-    if (/\d+/.test(colorKey.slice(colorKey.length - 1))) {
+    const colorValue = colors[colorKey];
+    if (/\d+/.test(colorKey.slice(colorKey.length - 1)) || !colorValue) {
       // Don't expand any colors that end in a number
       return extendedColors;
     }
@@ -81,13 +78,13 @@ const expandColors = (colors: IColorGuide, base?: IColorGuide): IColorGuide => {
       const valueNumber = padZeros(value * 100, 2);
       const valueKeyLight = `${colorKey}Light${valueNumber}`;
       // eslint-disable-next-line no-param-reassign
-      extendedColors[valueKeyLight] = base?.[valueKeyLight] || lighten(value, colors[colorKey]);
+      extendedColors[valueKeyLight] = colors?.[valueKeyLight] || lighten(value, colorValue);
       const valueKeyDark = `${colorKey}Dark${valueNumber}`;
       // eslint-disable-next-line no-param-reassign
-      extendedColors[valueKeyDark] = base?.[valueKeyDark] || darken(value, colors[colorKey]);
+      extendedColors[valueKeyDark] = colors?.[valueKeyDark] || darken(value, colorValue);
       const valueKeyClear = `${colorKey}Clear${valueNumber}`;
       // eslint-disable-next-line no-param-reassign
-      extendedColors[valueKeyClear] = base?.[valueKeyClear] || transparentize(value, colors[colorKey]);
+      extendedColors[valueKeyClear] = colors?.[valueKeyClear] || transparentize(value, colorValue);
     });
     return extendedColors;
   }, {});
