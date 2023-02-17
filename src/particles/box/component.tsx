e0ffc86a22b7ee57001f132b43d259694fedacf8
Krishan Patel <krishan711@gmail.com>
2023-02-17 16:02:52 +0000

    .

--- src/particles/box/component.tsx
+++ src/particles/box/component.tsx
@@ -12,10 +12,10 @@ interface IStyledBoxProps {
   $dimensions: IDimensionGuide;
   $height: ResponsiveField<string>;
   $width: ResponsiveField<string>;
-  $maxHeight: ResponsiveField<string>;
-  $maxWidth: ResponsiveField<string>;
-  $minHeight: ResponsiveField<string>;
-  $minWidth: ResponsiveField<string>;
+  $maxHeight: ResponsiveField<string> | null;
+  $maxWidth: ResponsiveField<string> | null;
+  $minHeight: ResponsiveField<string> | null;
+  $minWidth: ResponsiveField<string> | null;
   $blockType: string;
   $zIndex?: number;
 }
@@ -29,10 +29,10 @@ const StyledBox = styled.div<IStyledBoxProps>`
   box-sizing: border-box;
   ${(props: IStyledBoxProps): string => fieldToResponsiveCss(props.$height, props.$dimensions, getCss('height'))};
   ${(props: IStyledBoxProps): string => fieldToResponsiveCss(props.$width, props.$dimensions, getCss('width'))};
-  ${(props: IStyledBoxProps): string => fieldToResponsiveCss(props.$maxHeight, props.$dimensions, getCss('max-height'))};
-  ${(props: IStyledBoxProps): string => fieldToResponsiveCss(props.$maxWidth, props.$dimensions, getCss('max-width'))};
-  ${(props: IStyledBoxProps): string => fieldToResponsiveCss(props.$minHeight, props.$dimensions, getCss('min-height'))};
-  ${(props: IStyledBoxProps): string => fieldToResponsiveCss(props.$minWidth, props.$dimensions, getCss('min-width'))};
+  ${(props: IStyledBoxProps): string => (props.$maxHeight ? fieldToResponsiveCss(props.$maxHeight, props.$dimensions, getCss('max-height')) : '')};
+  ${(props: IStyledBoxProps): string => (props.$maxWidth ? fieldToResponsiveCss(props.$maxWidth, props.$dimensions, getCss('max-width')) : '')};
+  ${(props: IStyledBoxProps): string => (props.$minHeight ? fieldToResponsiveCss(props.$minHeight, props.$dimensions, getCss('min-height')) : '')};
+  ${(props: IStyledBoxProps): string => (props.$minWidth ? fieldToResponsiveCss(props.$minWidth, props.$dimensions, getCss('min-width')) : '')};
   display: ${(props: IStyledBoxProps): string => props.$blockType};
   flex-direction: column;
   z-index: ${(props: IStyledBoxProps): string => (props.$zIndex ? `${props.$zIndex}` : 'auto')};
@@ -78,10 +78,14 @@ export const Box = React.forwardRef((props: IBoxProps, ref: React.ForwardedRef<H
   const dimensions = useDimensions();
   const height = props.height || (props.isFullHeight ? '100%' : 'auto');
   const width = props.width || (props.isFullWidth ? '100%' : 'auto');
-  const maxHeight = props.maxHeight || 'none';
-  const maxWidth = props.maxWidth || 'none';
-  const minHeight = props.minHeight || 'auto';
-  const minWidth = props.minWidth || 'auto';
+  const maxHeight = props.maxHeight || null;
+  const maxWidth = props.maxWidth || null;
+  const minHeight = props.minHeight || null;
+  const minWidth = props.minWidth || null;
+  const maxHeightResponsive = props.maxHeightResponsive || maxHeight ? { base: (maxHeight || undefined), ...props.maxHeightResponsive } : null;
+  const maxWidthResponsive = props.maxWidthResponsive || maxWidth ? { base: (maxWidth || undefined), ...props.maxWidthResponsive } : null;
+  const minHeightResponsive = props.minHeightResponsive || minHeight ? { base: (minHeight || undefined), ...props.minHeightResponsive } : null;
+  const minWidthResponsive = props.minWidthResponsive || minWidth ? { base: (minWidth || undefined), ...props.minWidthResponsive } : null;
   const blockType = width === '100%' ? 'block' : 'flex';
 
   return (
@@ -92,10 +96,10 @@ export const Box = React.forwardRef((props: IBoxProps, ref: React.ForwardedRef<H
       $dimensions={dimensions}
       $height={{ base: height, ...props.heightResponsive }}
       $width={{ base: width, ...props.widthResponsive }}
-      $maxHeight={{ base: maxHeight, ...props.maxHeightResponsive }}
-      $maxWidth={{ base: maxWidth, ...props.maxWidthResponsive }}
-      $minHeight={{ base: minHeight, ...props.minHeightResponsive }}
-      $minWidth={{ base: minWidth, ...props.minWidthResponsive }}
+      $maxHeight={maxHeightResponsive}
+      $maxWidth={maxWidthResponsive}
+      $minHeight={minHeightResponsive}
+      $minWidth={minWidthResponsive}
       $blockType={blockType}
       $zIndex={props.zIndex}
       title={props.title}
