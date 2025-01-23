a87f5b72c1156bd3c5b024f3e5a084a411caac90
Krishan Patel <krishan711@gmail.com>
2025-01-23 17:20:04 +0000

    .

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,13 @@
 import path from 'path';
 
 export default config = {
-  framework: {
-    name: '@storybook/react-webpack5',
-  },
   stories: [
     '../src/*.@(mdx|stories.@(js|jsx|ts|tsx))',
     '../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
   ],
   staticDir: './.storybook/public',
   addons: [
-    '@storybook/addon-webpack5-compiler-babel',
+    // '@storybook/addon-webpack5-compiler-babel',
     '@storybook/addon-essentials',
     '@storybook/addon-links',
   ],
@@ -26,10 +23,4 @@ export default config = {
       configFile: path.resolve(__dirname, "./.babelrc.json"),
     }
   },
-  // webpackFinal: async (config, { configType }) => {
-  //   config.resolve = config.resolve || {};
-  //   config.resolve.fallback = config.resolve.fallback || {};
-  //   config.resolve.fallback.fs = false;
-  //   return config;
-  // },
 };
--- makefile
+++ makefile
@@ -30,7 +30,7 @@ security-check-ci:
 	@ echo "Not Supported"
 
 build:
-	@ npx build-react-component
+	@ npx build-module-rolldown
 
 build-ssr:
 	@ echo "Not Supported"
@@ -42,10 +42,10 @@ build-docs:
 	@ npx storybook build --docs --output-dir dist
 
 start:
-	@ npx build-react-component --start --dev
+	@ npx build-module-rolldown --start --dev
 
 start-prod:
-	@ npx build-react-component --start
+	@ npx build-module-rolldown --start
 
 start-docs:
 	@ npx storybook dev --docs --port 6006
