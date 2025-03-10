e5a4087830d9af976e0ecbe402d712442a1f61a4
Krishan Patel <krishan711@gmail.com>
2025-03-10 16:43:20 +0000

    .

--- src/wrappers/wrappingComponent.tsx
+++ src/wrappers/wrappingComponent.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 
 import { getClassName } from '@kibalabs/core';
-import { flattenChildren } from '@kibalabs/core-react';
+// import { flattenChildren } from '@kibalabs/core-react';
+import { isFragment } from 'react-is';
 
-import { IWrapperProps } from './wrapperProps';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ReactChild = (string | number | React.ReactElement<unknown, string | React.JSXElementConstructor<any>>);
+
+// https://github.com/grrowl/react-keyed-flatten-children
+export const flattenChildren = (children: React.ReactNode, depth = 0, keys: (string | number)[] = [], shouldRemoveEmptyStrings = true): (React.ReactElement | string | number)[] => {
+  console.log('flattenChildren', children);
+  return React.Children.toArray(children).reduce((acc: ReactChild[], node: React.ReactNode, nodeIndex: number) => {
+    if (isFragment(node)) {
+      // @ts-expect-error
+      acc.push(...flattenChildren((node as unknown as typeof React.Fragment).props.children, depth + 1, keys.concat(node.key || nodeIndex)));
+    } else if (React.isValidElement(node)) {
+      acc.push(React.cloneElement(node, { key: keys.concat(String(node.key)).join('.') }));
+    } else if (typeof node === 'number') {
+      acc.push(node);
+    } else if (typeof node === 'string' && (node || !shouldRemoveEmptyStrings)) {
+      acc.push(node);
+    }
+    return acc;
+  }, []);
+};
 
 const styleCopier = <P extends IWrapperProps>(props: P): React.ReactElement => {
-  const children = flattenChildren(props.children).map((child: (React.ReactElement | string | number)): (React.ReactElement | string | number) => {
+  console.log('styleCopier', props);
+  const flattenedChildren = flattenChildren(props.children);
+  console.log('flattenedChildren', flattenedChildren);
+  const children = flattenedChildren.map((child: (React.ReactElement | string | number)): (React.ReactElement | string | number) => {
     if (React.isValidElement(child)) {
       // @ts-ignore
       return React.cloneElement(child, { className: getClassName(child.props?.className, props.className) });
