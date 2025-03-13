335326529dd4714d1ac447f952dc2e7aa8cdc7ea
Krishan Patel <krishan711@gmail.com>
2025-03-13 14:08:09 +0000

    .

--- src/wrappers/wrappingComponent.tsx
+++ src/wrappers/wrappingComponent.tsx
@@ -1,29 +1,8 @@
 import React from 'react';
 
 import { getClassName } from '@kibalabs/core';
-// import { flattenChildren } from '@kibalabs/core-react';
-import { isFragment } from 'react-is';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type ReactChild = (string | number | React.ReactElement<unknown, string | React.JSXElementConstructor<any>>);
-
-// https://github.com/grrowl/react-keyed-flatten-children
-export const flattenChildren = (children: React.ReactNode, depth = 0, keys: (string | number)[] = [], shouldRemoveEmptyStrings = true): (React.ReactElement | string | number)[] => {
-  console.log('flattenChildren', children);
-  return React.Children.toArray(children).reduce((acc: ReactChild[], node: React.ReactNode, nodeIndex: number) => {
-    if (isFragment(node)) {
-      // @ts-expect-error
-      acc.push(...flattenChildren((node as unknown as typeof React.Fragment).props.children, depth + 1, keys.concat(node.key || nodeIndex)));
-    } else if (React.isValidElement(node)) {
-      acc.push(React.cloneElement(node, { key: keys.concat(String(node.key)).join('.') }));
-    } else if (typeof node === 'number') {
-      acc.push(node);
-    } else if (typeof node === 'string' && (node || !shouldRemoveEmptyStrings)) {
-      acc.push(node);
-    }
-    return acc;
-  }, []);
-};
+import { flattenChildren } from '@kibalabs/core-react';
+import { IWrapperProps } from './wrapperProps';
 
 const styleCopier = <P extends IWrapperProps>(props: P): React.ReactElement => {
   console.log('styleCopier', props);
